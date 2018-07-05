e32918620a503eab14136f5b4a85c0dfc8b50712
Jentoft <stefanjentoft@gmail.com>
2018-07-04 21:08:16 -0400

    First take on City Search

--- project2/src/app/Services/city.service.ts
+++ project2/src/app/Services/city.service.ts
@@ -9,13 +9,12 @@ import { Observable, of, ObservableLike } from 'rxjs';
 export class CityService {
 
   private zomatoUrl = 'https://developers.zomato.com/api/v2.1/cities?apikey=e44e4935c9867bb668311f76e5132541&q=';  // URL to web api
-  private city = 'abilene';
 
   constructor( private http: HttpClient,) { }
 
   //Service to view city with above url constants
-  citysearch(): Observable<City[]>{
-    return this.http.get<City[]>(this.zomatoUrl+this.city);
+  citysearch(cityName: string): Observable<City[]>{
+    return this.http.get<City[]>(this.zomatoUrl+cityName);
   }
 
 
--- project2/src/app/homepage/homepage.component.ts
+++ project2/src/app/homepage/homepage.component.ts
@@ -3,11 +3,15 @@ import { RestaurantService } from '../Services/restaurant.service';
 import { Restaurant } from '../restaurant';
 import { CityService } from '../Services/city.service';
 import { City } from '../city';
-import { Observable, of } from 'rxjs';
+import { Observable, Subject, of } from 'rxjs';
 import { Result } from '../Result';
 import { User } from '../user';
 import { restaurants } from '../restaurants';
 import { LoginService } from '../Services/login.service';
+import {
+  debounceTime, distinctUntilChanged, switchMap
+} from 'rxjs/operators';
+
 
 
 @Component({
@@ -18,17 +22,34 @@ import { LoginService } from '../Services/login.service';
 export class HomepageComponent implements OnInit {
 
   constructor(private restaurantservice: RestaurantService,
+           private cityService: CityService,
            private loginservice: LoginService) { }
 
+  search(term: string) {
+    this.searchTerms.next(term);
+  }
+  
   ngOnInit() {
     console.log(this.loginservice.loggedin)
+    this.cities$ = this.searchTerms.pipe(
+      // wait 300ms after each keystroke before considering the name
+      debounceTime(300),
+ 
+      // ignore new term if same as previous name
+      distinctUntilChanged(),
+ 
+      // switch to new search observable each time the term changes
+      switchMap((term: string) => this.cityService.citysearch(term)),
+    );
+ 
   }
   result: Result;
   city: number;
   cuisine: number; 
   loop: any;
   restaurants: restaurants;
-  
+  cities$: Observable<City[]>;
+  private searchTerms = new Subject<string>();
 
   //method to show restaurants meeting URL in restaurant service
   showRestaurants(): void {
@@ -40,5 +61,7 @@ export class HomepageComponent implements OnInit {
 
     document.getElementById("restaurantTable").style.display = "table";
   }
+
+
 
 }
