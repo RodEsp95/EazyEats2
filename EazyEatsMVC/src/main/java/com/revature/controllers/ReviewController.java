5635f16f7257b744ed864e0cb043a0d2d0060109
Rodrigo Espinosa <rod_elmariachi@Rodrigos-MacBook-Pro.local>
2018-07-05 17:37:25 -0400

    Added DELETE method in Review Controller

--- EazyEatsMVC/src/main/java/com/revature/controllers/ReviewController.java
+++ EazyEatsMVC/src/main/java/com/revature/controllers/ReviewController.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -107,6 +108,17 @@ public class ReviewController {
 	@ResponseBody
 	public List<Review> getReviewsRestaurantId(@PathVariable("id") Integer id){
 		return rdi.getReviewsByRestaurantId(id);
+	}
+	
+	@CrossOrigin(origins="http://localhost:4200", methods=RequestMethod.DELETE)
+	@DeleteMapping("/reviews/{id}")
+	@ResponseBody
+	public String deleteReview(@PathVariable("id") int id) {
+		Review review = rdi.getReviewById(id);
+		if(review != null) {
+			rdi.deleteReviewById(id);
+		}
+		return "Review deleted";
 	}
 	
 
